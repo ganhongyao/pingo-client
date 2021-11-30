70bf8eb8dce33c613c25b46c0f550af33ddc3516
Hong Yao <ganhongyao@u.nus.edu>
2021-12-01 00:01:13 +0800

    Add drawer

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,18 +1,46 @@
-import { Typography } from "@mui/material";
+import { AppBar, Box, CssBaseline, Toolbar, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import AppDrawer from "../components/AppDrawer";
 import useGeoLocation from "../hooks/useGeoLocation";
 
+const useStyles = makeStyles((theme) => ({
+  header: {
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
+
 function Dashboard() {
+  const classes = useStyles();
   const location = useGeoLocation();
+
   if (!location) {
     return <div>Location not enabled.</div>;
   }
 
   return (
-    <div>
-      <Typography variant="h1">Dashboard</Typography>
-      <Typography variant="h3">Latitude: {location.latitude}</Typography>
-      <Typography variant="h3">Longitude: {location.longitude}</Typography>
-    </div>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar className={classes.header}>
+          <Typography variant="h6" noWrap component="div" align="right">
+            Pingo
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <AppDrawer />
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+      >
+        <Toolbar />
+        <Typography paragraph>Latitude: {location.latitude}</Typography>
+        <Typography paragraph>Longitude: {location.longitude}</Typography>
+      </Box>
+    </Box>
   );
 }
 
