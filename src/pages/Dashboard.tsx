6206eab540fc2a9abfefafcd317284bed9740bfc
Hong Yao <ganhongyao@u.nus.edu>
2021-12-02 23:49:16 +0800

    Add useSocket hook

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,23 +4,19 @@ import { useEffect, useState } from "react";
 import { Viewport } from "../types/viewport";
 import { GeoLocation } from "../types/geolocation";
 import { DEFAULT_MAP_CENTER, MAPBOX_STYLE } from "../util/constants";
-import socketIOClient from "socket.io-client";
+import { useSocket } from "../hooks/useSocket";
+import { useSnackbar } from "notistack";
 
 function Dashboard() {
   const location = useGeoLocation();
+  const socket = useSocket();
+  const { enqueueSnackbar } = useSnackbar();
   const [viewport, setViewport] = useState<Viewport>({
     ...DEFAULT_MAP_CENTER,
     width: "75vw",
     height: "80vh",
     zoom: 12,
   });
-
-  useEffect(() => {
-    const socket = socketIOClient("http://localhost:4001");
-    socket.on("friend connection", (data) => {
-      console.log("a friend connected");
-    });
-  }, []);
 
   useEffect(() => {
     if (location) {
@@ -31,6 +27,16 @@ function Dashboard() {
       }));
     }
   }, [location]);
+
+  useEffect(() => {
+    socket?.on("friend connection", () => {
+      console.log("friend connected");
+      enqueueSnackbar("Friend is online!", { variant: "success" });
+    });
+    return () => {
+      socket?.off("friend connection");
+    };
+  });
 
   if (!location) {
     return <div>Location not enabled.</div>;
