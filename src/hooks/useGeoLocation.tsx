fd72d3836e719e592f8bc90bceab124d12c90c37
Hong Yao <ganhongyao@u.nus.edu>
2021-12-06 00:15:10 +0800

    Refactor useGeoLocation hook

--- src/hooks/useGeoLocation.tsx
+++ src/hooks/useGeoLocation.tsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GeoLocation } from "../types/geolocation";
 import { Nullable } from "../types/nullable";
 
 export default function useGeoLocation(): Nullable<GeoLocation> {
   const [location, setLocation] = useState<Nullable<GeoLocation>>(null);
 
-  navigator.geolocation.getCurrentPosition((pos) => {
+  const onChange = ({ coords }: GeolocationPosition) => {
     setLocation({
-      latitude: pos.coords.latitude,
-      longitude: pos.coords.longitude,
+      latitude: coords.latitude,
+      longitude: coords.longitude,
     });
-  });
+  };
+
+  useEffect(() => {
+    const geoloc = navigator.geolocation;
+    if (!geoloc) {
+      return;
+    }
+
+    const watcher = geoloc.watchPosition(onChange);
+    return () => {
+      geoloc.clearWatch(watcher);
+    };
+  }, []);
 
   return location;
 }
